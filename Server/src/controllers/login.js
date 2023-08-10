634df1e001afad4a88fcf0235c72efc7eb686557
raulsedano2410 <raulsedanomolina@mal.com>
2023-08-09 21:06:21 -0500

    login

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,12 +1,36 @@
-const users = require('../utils/user')
+// const users = require('../utils/user')
 
-function login(req, res) {
-    const { email, password } = req.query;
+// function login(req, res) {
+//     const { email, password } = req.query;
 
-    const access =
-        users.some((user) => user.email === email && user.password === password);
+//     const access =
+//         users.some((user) => user.email === email && user.password === password);
 
-    return res.status(200).json({ access });
-};
+//     return res.status(200).json({ access });
+// };
 
+// module.exports = login;
+
+const { User } = require("../DB_connection");
+
+async function login(req, res) {
+    try {
+        const { email, password } = req.query;
+        if (!email || !password) {
+            return res.status(400).json({ error: "Missing data" });
+        }
+        const user = await User.findOne({
+            where: { email },
+        });
+        if (!user) {
+            return res.status(404).json({ error: "User not fond" });
+        }
+        if (user.password !== password) {
+            return res.status(403).json({ error: "Incorrect password" });
+        }
+        return res.status(200).json({ access: true });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
 module.exports = login;
